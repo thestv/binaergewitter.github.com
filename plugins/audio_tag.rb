24f4df01d0c84f834e37cd1e0558e8952b4d2a04
Sven Pfleiderer <sg+github@roothausen.de>
2012-02-02 18:57:08 +0100

    Implement first version of multiformat audio tag plugin

--- plugins/audio_tag.rb
+++ plugins/audio_tag.rb
@@ -2,14 +2,15 @@
 # Author: Sven Pfleiderer http://blog.roothausen.de
 # Description: Easily output HTML5 audio
 #
-# Syntax {% audio url/to/audio %}
+# Syntax {% audio url/to/audio, [, url/to/audio2] %}
 #
 # Example:
-# {% audio http://site.com/audio.mp3 %}
+# {% audio http://site.com/audio.mp3, http://site.com/audio.ogg %}
 #
 # Output:
 # <audio preload='none' controls>
 #   <source src='http://site.com/audio.mp3' type='audio/mp3' />
+#   <source src='http://site.com/audio.ogg' type='audio/ogg' />
 # </audio>
 
 require 'uri'
@@ -17,7 +18,10 @@ require 'uri'
 module Jekyll
 
   class AudioTag < Liquid::Tag
-    
+
+    UrlSyntax = /(((\s*,?)(https?:\/\/)(\S+))+)/
+    VarSyntax = /((\.?(\w+))*)/
+
     def initialize(tag_name, markup, tokens)
       @markup = markup
       super
@@ -26,12 +30,17 @@ module Jekyll
     def render(context)
       output = super
 
-      if @markup =~ /((https?:\/\/|\/)(\S+))/i
-        @audio = $1
-      elsif @markup =~ /([\w]+(\.[\w]+)*)/i
+      if @markup =~ UrlSyntax
+        @audio = $1.split(",")
+      elsif @markup =~ VarSyntax
         # Liquid does not expand variables on custom tags inside templates 
         # so we need to look it up manually
-        @audio = look_up(context, $1)
+        lookup = context[$1]
+        if lookup.is_a?(Hash)
+          @audio = lookup.values
+        else
+          @audio = Array(lookup)
+        end
       end
 
       generate_html
@@ -39,24 +48,20 @@ module Jekyll
 
     private
 
-    def look_up(context, name)
-      lookup = context
-
-      name.split(".").each do |value|
-        lookup = lookup[value]
-      end
-
-      lookup
-    end
-
     def generate_html
       if @audio
-        mime_type = get_mime_type(@audio)
-        audio =  "<audio preload='none' controls>"
-        audio += "<source src='#{@audio}' type='#{mime_type}'/></audio>"
+        player_html = "<audio preload='none' controls>"
+
+        @audio.each do |audio_url|
+          mime_type = get_mime_type(audio_url)
+          player_html += "<source src='#{audio_url}' type='#{mime_type}' />"
+        end
+
+        player_html += "</audio>"
       else
-        "Error processing input, expected syntax: {% audio url/to/audio %}"
+        "Error processing input, Syntas: {% audio url/to/audio, [, url/to/audio2] %}"
       end
+
     end
 
     def get_mime_type(audio)
