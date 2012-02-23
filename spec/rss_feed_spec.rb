09cb725678d089851c7bac94d458edab6d2e6b8d
Marc Seeger <mail@marc-seeger.de>
2012-02-23 20:43:41 +0100

    Switch from MarkupValidator to FeedValidator

--- spec/rss_feed_spec.rb
+++ spec/rss_feed_spec.rb
@@ -3,7 +3,7 @@ require 'w3c_validators'
 
 describe 'RSSFeed' do
 	it "should be valid XML" do
-		@validator = W3CValidators::MarkupValidator.new
+		@validator = W3CValidators::FeedValidator.new
 		file = File.dirname(__FILE__) + '/../public/podcast.xml'
 		results = @validator.validate_file(file)
 		results.errors.should == []
